f4c5a702ef70c898e220632aab3442fd2712b9c6
aryaputrabip <aryaputrabip@gmail.com>
2020-01-08 18:52:15 +0700

    second commit of prima number

--- Number.java
+++ Number.java
@@ -2,7 +2,7 @@ public class Number {
 	public static void main(String args[]){
 		int a = 0;
 		
-		System.out.println("Mencari Bilangan Prima");
+		System.out.println("Mencari Bilangan Prima V2");
 		for(int i=0;i<=100;i++){
 			if(i%0 == 0){
 				a = a+1;
@@ -10,7 +10,6 @@ public class Number {
 			if(a == 2){
 				System.out.println(i = " adalah bilangan prima");
 			}
-			
 		}
 	}
 }
