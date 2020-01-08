725eeb7d1dc1f3525bdc6cd33c0ac24fc995bc3a
aryaputrabip <aryaputrabip@gmail.com>
2020-01-08 12:40:56 +0700

    second commit changes

--- Number.java
+++ Number.java
@@ -1,7 +1,7 @@
 public class Number {
 	public static void main(String args[]){
 		System.out.println("Hello World");
-		for(int i=0;i<100;i++){
+		for(int i=0;i<50;i++){
 			System.out.println("Try Number " +i +" times");
 		}
 	}
