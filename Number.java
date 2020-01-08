1c91f57fec1a8646d42b3339eb61976995884da8
aryaputrabip <aryaputrabip@gmail.com>
2020-01-08 19:42:06 +0700

    8th commit

--- Number.java
+++ Number.java
@@ -3,9 +3,9 @@ public class Number {
 		bool isPrima = false;
 		
 		System.out.println("Mencari Bilangan Prima V2");
-		for(int i=0;i<=100;i++){
-			for(int j=2; j<i;j++){
-				if(i%j == 0){
+		for(int i=0;i<100;i++){
+			for(int j=2;j<=i/2; j++){
+				if(i % j == 0){
 					isPrima = false;
 					break;
 				}else{
