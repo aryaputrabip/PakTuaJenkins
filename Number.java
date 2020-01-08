b41f9d80867b1223803d3197033024a8f798759d
aryaputrabip <aryaputrabip@gmail.com>
2020-01-08 19:24:17 +0700

    fifth commit prima

--- Number.java
+++ Number.java
@@ -1,12 +1,16 @@
 public class Number {
 	public static void main(String args[]){
-		int a = 0;
+		bool isPrima = false;
 		
 		System.out.println("Mencari Bilangan Prima V2");
 		for(int i=0;i<=100;i++){
 			for(int j=2; j<i;j++){
 				if(i%j>0){
+					isPrima = true;
+				}
+				if(isPrima){
 					System.out.println(i + " adalah bilangan prima.");
+					isPrima = false;
 				}
 			}
 		}
