0e80ce1dba4c36b3758509842f3663ea3181a847
aryaputrabip <aryaputrabip@gmail.com>
2020-01-08 19:20:57 +0700

    fourth commit prima

--- Number.java
+++ Number.java
@@ -4,11 +4,10 @@ public class Number {
 		
 		System.out.println("Mencari Bilangan Prima V2");
 		for(int i=0;i<=100;i++){
-			if(i%0 == 0){
-				a = a+1;
-			}
-			if(a == 2){
-				System.out.println(i + " adalah bilangan prima.");
+			for(int j=2; j<i;j++){
+				if(i%j>0){
+					System.out.println(i + " adalah bilangan prima.");
+				}
 			}
 		}
 	}
